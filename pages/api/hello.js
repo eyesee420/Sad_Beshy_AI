743dc218ccf2485fa86fc6fcbf520601dee0ae07
eyesee <icaonapo12345@gmail.com>
2023-07-14 03:24:30 +0800

    initial

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Configuration, OpenAIApi } from 'openai';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const configuration = new Configuration({
+  apiKey:process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const handler = async (req, res) => {
+  const prompt = req.query.prompt;
+
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `make me a super motivational cringy quote in tagalog  about my friend for example
+      ${prompt}
+      that starts with: malungkot ang beshy ko kasi ${prompt}  `,
+      max_tokens: 300,
+      temperature: 1,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+    });
+
+    const quote = completion.data.choices[0].text;
+
+    res.status(200).json({ quote });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export default handler;

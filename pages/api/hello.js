cdf0e73ba510aae8f0ad765cf812afee02dd3675
ian christopher icaonapo <75460564+eyesee420@users.noreply.github.com>
2023-07-14 03:27:32 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey:process.env.OPENAI_API_KEY,
+  apiKey:"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",
 });
 
 const openai = new OpenAIApi(configuration);
